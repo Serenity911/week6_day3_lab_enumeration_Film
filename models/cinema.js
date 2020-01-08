2c77bde6ecf1654a1ee921da1c94d582590d204d
Serenity911 <silvia.simonassi01@gmail.com>
2020-01-08 14:13:52 +0000

    cinema test passes     ✓ should be able to find a film by title

--- models/cinema.js
+++ models/cinema.js
@@ -4,9 +4,17 @@ const Cinema = function (films) {
 
 Cinema.prototype.get_titles = function () {
   const filmTitles = this.films.map((film) => {
-      return film.title;
-    });
-    return filmTitles;
+    return film.title;
+  });
+  return filmTitles;
 };
+
+Cinema.prototype.find = function(title) {
+  const filmByTitle = this.films.find((film) => {
+    return film.title === title;
+  })
+  return filmByTitle;
+};
+
 
 module.exports = Cinema;
